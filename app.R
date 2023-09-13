d1792cdc2652c35a2193bb273af8bcd88bee5226
farfp-dpma <144616952+farfp-dpma@users.noreply.github.com>
2023-09-13 09:53:03 +0200

    Add files via upload

---
+++ app.R
@@ -0,0 +1,21 @@
+shinyApp(
+  ui = dashboardPage(
+    header = dashboardHeader(),
+    sidebar = dashboardSidebar(),
+    body = dashboardBody(
+      tabBox(
+        id = "tabset1",
+        height = 750,
+        #width = 12,
+        tabPanel("Hello", "This is the hello tab",
+                 div(style = 'overflow-x: scroll', DT::dataTableOutput('myTable'))
+        ))
+    )
+  ),
+  server = function(input, output, session) {
+    output$myTable <- DT::renderDT({
+      DT::datatable(
+        mtcars) 
+    })
+  }
+)
